28aaa8d4edf39086a9157b1ecb5ddce3c033e60c
Tharindu-Deshan <deshantharindu63@gmail.com>
2026-07-07 15:22:06 +0530

    notification app initialization. (initial stable version)

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -7,7 +7,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
   imports: [
     ClientsModule.register([
       {
-        name: 'AUTH',
+        name: 'AUTH_SERVICE',
         transport: Transport.TCP,
         options: { host: '127.0.0.1', port: 3001 },
       },
---
+++ apps/notification/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { NotificationModule } from './notification.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const notification_app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationModule,{
+      transport:Transport.TCP,
+      options:{host:'127.0.0.1',port:3002}
+    }
+  );
+  await notification_app.listen();
+}
+bootstrap();
